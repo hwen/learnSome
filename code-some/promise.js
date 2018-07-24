1353685c6734d799befbd325d140ba8764227180
hwen <hwenleung@gmail.com>
2018-07-24 09:13:01 +0800

    promise test

--- code-some/promise.js
+++ code-some/promise.js
@@ -1,7 +1,7 @@
-const getAsync = () => {
+const getAsync = (params) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve({ a: 1 });
+      resolve({ a: params });
     }, 500);
   });
 };
@@ -17,8 +17,22 @@ const getB = () => {
   });
 };
 
+// getB then 里可以改变 resp 的值
 getB().then(resp => {
   console.log('======== after ==========');
   console.log(resp);
   // 输出 { b: { a:1 }, test: 'hallo' }
 });
+
+// 测试 async/await 在 for 循环里使用
+async function testFor() {
+  const result = [];
+  for (let i = 0; i < 11; i++) {
+    const resp = await getAsync(i);
+    result.push(resp)
+  }
+  console.log('======= async/await with for =========')
+  console.log(result)
+}
+
+testFor();
